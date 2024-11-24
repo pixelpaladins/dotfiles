00cc0854b0b5bd44c6ab576677ef4d0e2e1d24a8
Lauren Lajas <29675370+llajas@users.noreply.github.com>
2024-11-24 10:12:18 -0600

    update key reference (OpenAI)

--- dot_dotfiles/exports/genericExports.zsh
+++ dot_dotfiles/exports/genericExports.zsh
@@ -1,5 +1,5 @@
 ## ~/.dotfiles/exports/genericExports.zsh
 
 #OpenAI
-export OPENAI_KEY=`creds "lixusyn5hlqzelvbias36c6vka" "apiKey"`
+export OPENAI_KEY=`creds "572rjdqjyon3zvk3mvgmumjeba" "apiKey"`
 
