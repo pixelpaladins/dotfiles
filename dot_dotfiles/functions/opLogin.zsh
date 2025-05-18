e734f86f5cdf249c8281eeb058b96c45557a976f
Lauren Lajas <29675370+llajas@users.noreply.github.com>
2025-05-18 11:18:06 -0500

    fix: oplogin

--- dot_dotfiles/functions/opLogin.zsh
+++ dot_dotfiles/functions/opLogin.zsh
@@ -1,7 +1,14 @@
 # ~/.dotfiles/functions/opLogin.zsh
 
 # Login to 1Password CLI
+# ~/.dotfiles/functions/opLogin.zsh
 opLogin () {
-    local account=$(op account list --format json | jq -r '.[0].user_uuid')
-    eval "$(op signin --account "$account" --raw)"
+  # grab the first account’s shorthand – NOT its user_uuid
+  local acct=$(op account list --format=json | jq -r '.[0].shorthand')
+
+  # Option A: classic – let 1Password emit the export line
+  eval "$(op signin --account "$acct")"
+
+  # Option B: raw token – do the export yourself
+  # export OP_SESSION_${acct}="$(op signin --account "$acct" --raw)"
 }
