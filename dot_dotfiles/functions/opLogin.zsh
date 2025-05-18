e20f5883b9e8f6674b65b75f2545343fed4fb023
Lauren Lajas <29675370+llajas@users.noreply.github.com>
2025-05-18 03:28:37 -0500

    Improve opLogin function for 1Password CLI sign-in

--- dot_dotfiles/functions/opLogin.zsh
+++ dot_dotfiles/functions/opLogin.zsh
@@ -1,6 +1,7 @@
 # ~/.dotfiles/functions/opLogin.zsh
 
 # Login to 1Password CLI
-opLogin() {
-  eval $(op signin my)
+opLogin () {
+    local account=$(op account list --format json | jq -r '.[0].user_uuid')
+    eval "$(op signin --account "$account" --raw)"
 }
